26978626f49ec1a55218ff598a53888a41aaa975
Reece English <englishra.dev@icloud.com>
2023-03-20 17:45:42 +0000

    - Ammended it so that only Auth0 actions is able to create a user - Added validation

--- iDSimplify_User_Service/index.mjs
+++ iDSimplify_User_Service/index.mjs
@@ -4,14 +4,17 @@ import validateJSONWScheme from './JSONValidator.mjs';
 import schemas from './schemas.mjs';
 
 const ddbClient = new DynamoDBClient({ region: 'eu-west-2' });
-const userTable = process.env.USER_TABLE;
+
+// Environment variables
+const USER_TABLE = process.env.USER_TABLE;
+const AUTH0_CLIENT_ID = process.env.AUTH0_CLIENT_ID;
 
 export const handler = async (event) => {
     let response;
 
     switch (true) {
-        case event.httpMethod === 'POST':
-            response = createUser(JSON.parse(event.body));
+        case event.httpMethod === 'POST' && event.path === '/user':
+            response = createUser(JSON.parse(event.body), event.requestContext);
             break;
         default:
             response = buildResponse(404, '404 Not Found');
@@ -20,24 +23,52 @@ export const handler = async (event) => {
     return response;
 };
 
-const createUser = async (requestBody) => {
+const createUser = async (requestBody, requestContext) => {
+
+    // Ensure this request is only coming from Auth0
+    const clientID = requestContext.authorizer.principalId;
+    if (clientID != `${AUTH0_CLIENT_ID}@clients`) { return buildResponse(401, 'Not Auth0'); }
 
     // Validate that the data is formatted correctly
     const isDataValid = validateJSONWScheme(requestBody, schemas['user']);
     if (!isDataValid) { return buildResponse(400, 'Incorrect Data'); }
 
-    const params = {
-        TableName: userTable,
-        Item: requestBody
+    // Build the item
+    const userItem = {
+        userId: requestBody.userId,
+        createdAt: requestBody.createdAt
+    };
+
+    // Build the DB request
+    const dbRequest = {
+        TableName: USER_TABLE,
+        Item: userItem,
+        ConditionExpression: "userId <> :userIdValue",
+        ExpressionAttributeValues: {
+            ":userIdValue": requestBody.userId
+        }
     };
 
     try {
-        const data = await ddbClient.send(new PutCommand(params));
-        console.log('Success - item added or updated', data);
-        const body = { Operation: 'SAVE', Message: 'SUCCESS', Item: requestBody };
-        return buildResponse(200, body);
-    } catch (err) {
+        // Save data to the DB
+        const data = await ddbClient.send(new PutCommand(dbRequest));
+
+        // Successful save - Build the response
+        const responseBody = {
+            Operation: 'SAVE',
+            Message: 'User created successfully',
+            Item: userItem
+        };
+
+        // Send back response
+        return buildResponse(200, responseBody);
+    }
+    catch (err) {
+        // An error occurred in saving to the DB
         console.log('Error', err.stack);
+
+        // Send back response
+        return buildResponse(500, 'Unable to create user');
     }
 };
 
--- iDSimplify_User_Service/schemas.mjs
+++ iDSimplify_User_Service/schemas.mjs
@@ -7,19 +7,15 @@ const schemas = {
                 "description": "The PK of the user, as define by Auth0",
                 "type": "string"
             },
-            "email": {
-                "title": "Email address",
-                "description": "The email address of the user",
-                "type": "string",
-                "pattern": "^\\S+@\\S+\\.\\S+$",
-                "minLength": 6,
-                "maxLength": 127
+            "createdAt": {
+                "description": "The date in epoch time, that the user was created",
+                "type": "string"
             }
         },
         "additionalProperties": false,
         "required": [
             "userId",
-            "email"
+            "createdAt"
         ]
     }
 };
