75225b8347eb08899a8aa39552ee9cbd0404caa1
Reece English <englishra.dev@icloud.com>
2023-04-02 15:51:32 +0100

    - Reset password integration added

--- iDSimplify_Azure_Integration_Service/index.mjs
+++ iDSimplify_Azure_Integration_Service/index.mjs
@@ -13,6 +13,9 @@ export const handler = async (event) => {
         case event.httpMethod === 'GET' && event.resource === '/integrations/users/{id}/groups':
             response = getUserGroups(event);
             break;
+        case event.httpMethod === 'POST' && event.resource === '/integrations/users/{id}/resetpassword':
+            response = resetPassword(event);
+            break;
         default:
             response = buildResponse(404, '404 Not Found');
     }
@@ -137,6 +140,46 @@ const getUserGroups = async (event) => {
     }
 
     return buildResponse(200, userData);
+};
+
+
+const resetPassword = async (event) => {
+
+    // TODO: Confirm that the requesting user is authorised to perform this action
+
+    // Get the id of the user being queried
+    const pathParameters = event.pathParameters;
+    const queriedUserID = pathParameters.id;
+
+    // Get the relevant integration details
+    const tenantID = '58cf20ee-3772-4478-9af3-d1972f80609c';
+    const clientID = 'b2d3f318-1ae0-4a2b-b62e-e33d8f9cd8d8';
+    const clientSecret = 'EUQ8Q~07deBRW1HGFv9E1BNA3oDxdcmDpft5Sbek';
+
+    // Get the access token for the Graph API and confirm it's valid
+    var azureAccessToken = await getAzureAccessToken(tenantID, clientID, clientSecret);
+    if (azureAccessToken === undefined || azureAccessToken === null) { return buildResponse(401, 'Unable to authenticate with Azure'); }
+
+    // Query the Azure Graph API
+    var responseData = null;
+    try {
+        var headers = new Headers();
+        headers.append("Authorization", `Bearer ${azureAccessToken}`);
+
+        var requestOptions = {
+            method: 'POST',
+            headers: headers
+        };
+
+        const response = await fetch(`https://graph.microsoft.com/v1.0/users/${queriedUserID}/authentication/methods/28c10230-6103-485e-b985-444c60001490/resetPassword`, requestOptions);
+        responseData = await response.json();
+    }
+    catch (e) {
+        console.log(e);
+        return buildResponse(400, 'Failed to query Azure');
+    }
+
+    return buildResponse(200, responseData);
 };
 
 
