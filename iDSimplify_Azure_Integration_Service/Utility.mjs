7e72b79de616a395d9747702c2134a8ea82fbc9e
Reece English <englishra.dev@icloud.com>
2023-04-08 14:50:03 +0100

    - Making it more modular - Testing disable account

---
+++ iDSimplify_Azure_Integration_Service/Utility.mjs
@@ -0,0 +1,11 @@
+export function buildResponse(statusCode, body) {
+    return {
+        statusCode: statusCode,
+        headers: {
+            'Content-Type': 'application/json',
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Credentials": true // Required for cookies, authorization headers with HTTPS
+        },
+        body: JSON.stringify(body)
+    }
+}
